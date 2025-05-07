476682598f11ff517908b375e106673a725ffa86
sasetz <56696451a@gmail.com>
2025-05-07 14:04:52 +0200

    Remove BEU dont_touch attribute

--- rtl/executor.sv
+++ rtl/executor.sv
@@ -83,7 +83,7 @@ module executor (
         .s_result_o(s_mdu_result)
     );
 
-    (* dont_touch = "yes" *) beu m_beu
+    beu m_beu
     (
         .s_function_i(s_function_i),
         .s_compare_i(s_ictrl_i[ICTRL_UNIT_ALU]),
